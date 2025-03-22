af485cfc99e93ff64acc091424cd1dabf088523c
Joshualaycoh <your_email@example.com>
2025-03-22 16:46:01 +0800

    first commit

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("public"))
-app.use(bodyParser.urlencoded({extended: true}))
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-
-app.get("/", (req,res) => {
-  res.render("index.ejs")
-})
-
-app.post("/submit", async (req,res) => {
+app.post("/submit", async (req, res) => {
   const { location } = req.body;
- 
-   // Check if location is defined
-   if (!location) {
-    console.log('Location is not defined:', req.body);
-    return res.status(400).send('Location is required.');
-  }
-  
 
-  try{
-    // Convert location to lat, lng using geocoding API
-    const geoResponse = await axios.get(`http://api.positionstack.com/v1/forward?access_key=45f50e43f2a39ff18c1b6a53f2e2ee9a&query=${location}`);
+  if (!location) {
+    console.log("Location is not defined:", req.body);
+    return res.status(400).send("Location is required.");
+  }
+
+  try {
+    const geoResponse = await axios.get(
+      `http://api.positionstack.com/v1/forward?access_key=${process.env.POSITIONSTACK_API_KEY}&query=${location}`
+    );
     const { latitude: lat, longitude: lng } = geoResponse.data.data[0];
 
-    const response = await axios.get(`https://api.openuv.io/api/v1/uv?lat=${lat}&lng=${lng}`, {
-      headers: {
-        'x-access-token': 'openuv-15gjb5grlvssbc14-io'
+    const response = await axios.get(
+      `https://api.openuv.io/api/v1/uv?lat=${lat}&lng=${lng}`,
+      {
+        headers: {
+          "x-access-token": process.env.OPENUV_API_KEY,
+        },
       }
-    });
-    res.render("index.ejs", { uvIndex: response.data.result.uv })
-
-  }catch (error) {
+    );
+    res.render("index.ejs", { uvIndex: response.data.result.uv });
+  } catch (error) {
     console.error(error);
-    res.status(500).send('An error occurred while fetching UV index data.');
+    res.status(500).send("An error occurred while fetching UV index data.");
   }
-})
+});
 
-app.listen( port, ()=> {
-    console.log(`Listening from port ${port}.`)
-})
+app.listen(port, () => {
+  console.log(`Listening from port ${port}.`);
+});
